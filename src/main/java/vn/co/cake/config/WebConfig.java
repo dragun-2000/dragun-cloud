3240439b3a23f36d4d4ac7a5fcbc0ca6a783bbb7
Dragun <trinhhai28041994@gmail.com>
2025-10-15 22:49:37 +0700

    add CDN, minio S3

--- src/main/java/vn/co/cake/config/WebConfig.java
+++ src/main/java/vn/co/cake/config/WebConfig.java
@@ -2,6 +2,7 @@ package vn.co.cake.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,9 +11,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://www.phodem.click")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://www.phodem.click",
+                        "https://dragun.cloud",
+                        "https://debase.vn"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                // Cache static file 30 ngày
+                .setCachePeriod(2592000); // 30 * 24 * 60 * 60
     }
 }
