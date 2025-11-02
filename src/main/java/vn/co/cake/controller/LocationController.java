18a231b8ea715887a120da5bffe7a6f73f580c49
Dragun <trinhhai28041994@gmail.com>
2025-11-02 22:19:13 +0700

    add provinces

--- src/main/java/vn/co/cake/controller/LocationController.java
+++ src/main/java/vn/co/cake/controller/LocationController.java
@@ -5,15 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import vn.co.cake.entity.District;
+import vn.co.cake.entity.Province;
 import vn.co.cake.entity.Ward;
 import vn.co.cake.repository.DistrictRepository;
 import vn.co.cake.repository.WardRepository;
+import vn.co.cake.repository.ProvinceRepository;
 import vn.co.cake.utils.MasterUtil;
 
 import java.util.List;
 
 @Controller
 public class LocationController {
+
+    private final ProvinceRepository provinceRepository;
 
     private final DistrictRepository districtRepository;
 
@@ -22,10 +26,18 @@ public class LocationController {
     private final MasterUtil masterUtil;
 
     public LocationController(DistrictRepository districtRepository,
-                              WardRepository wardRepository, MasterUtil masterUtil) {
+                              WardRepository wardRepository, MasterUtil masterUtil, 
+                              ProvinceRepository provinceRepository) {
         this.districtRepository = districtRepository;
         this.wardRepository = wardRepository;
         this.masterUtil = masterUtil;
+        this.provinceRepository = provinceRepository;
+    }
+
+    @GetMapping("/provinces")
+    @ResponseBody
+    public List<Province> getDistrictsByProvince() {
+        return provinceRepository.findAll();
     }
 
     @GetMapping("/districts/{provinceCode}")
